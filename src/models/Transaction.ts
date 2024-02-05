d803f19ba9cb9c3c71a5d1a73e531a1fe354e823
Gerard <gerard@protofy.xyz>
2024-02-05 20:49:57 +0100

    [ADD/WIP] Create transaction model and some getters

---
+++ src/models/Transaction.ts
@@ -0,0 +1,100 @@
+
+import { v4 as uuidv4 } from 'uuid';
+
+export interface TransactionType {
+    id: string,
+    operation: "WITHDRAW" | "TRANSFER" | "DEPOSIT",
+    amount: number,
+    timestamp: number,
+    fromAccountId: string,
+    toAccountId?: string // optional, necessary data only for "TRANSFER"
+}
+
+export class Transaction {
+    private readonly data: TransactionType;
+    
+    constructor(data: TransactionType) {
+        this.data = data;
+    }
+
+    static load(data: TransactionType): Transaction {
+        return new Transaction(data)
+    }
+
+    getData(): TransactionType {
+        return this.data;
+    }
+
+    get(key: keyof TransactionType): any {
+        return this.getData()[key]
+    }
+
+    getId(): string {
+        return this.get("id")
+    }
+
+    getAmount(): number {
+        return this.get("amount")
+    }
+
+    getFromAccount(): number {
+        return this.get("fromAccountId")
+    }
+
+    getToAccount(): number {
+        try {
+            return this.get("toAccountId")
+        }catch(e) {
+            throw "This transaction has no desinary identifier (toAccountId)"
+        }
+    }
+
+    getOperationType(): keyof TransactionType["operation"] {
+        return this.get('operation')
+    }
+
+    getTimestamp(): number {
+        return this.get('timestamp')
+    }
+
+    static create( // WIP
+        operation: TransactionType["operation"], 
+        amount: number,
+        fromAccountId: string,
+        toAccountId?: string,
+        id?: string
+        ): Transaction {
+        const newTransactionData: TransactionType = {
+            id: id??uuidv4(),
+            operation: operation,
+            amount,
+            fromAccountId,
+            toAccountId,
+            timestamp:  Date.now()
+        }
+        return Transaction.load(newTransactionData)
+    }
+}
+
+
+export class TransactionCollection { // WIP
+    private readonly items: Transaction[];
+    constructor(items: Transaction[]) {
+        this.items = items;
+    }
+    static load(items: Transaction[]): TransactionCollection {
+        return new TransactionCollection(items)
+    }
+
+    getItems(): Transaction[] {
+        return this.items;
+    }
+
+    length(): number {
+        return this.getItems().length;
+    }
+
+    // getDailyTransactions(): TransactionCollection{
+    //     return 
+    // }
+}
