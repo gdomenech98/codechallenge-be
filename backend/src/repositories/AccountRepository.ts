516f22ecf61c608e087baf38198064dc07044175
Gerard <gerard@protofy.xyz>
2024-02-06 00:33:04 +0100

    [ADD] Transaction and account Repositories

--- backend/src/repositories/AccountRepository.ts
+++ backend/src/repositories/AccountRepository.ts
@@ -1,6 +1,36 @@
-// import { Account } from "../models/Account";
+import { MongoDB } from "../connectors/db";
+import { AccountType } from "../models/Account";
 
-// interface AccountRepository {
-//     save(account: Account): Promise<void>;
-//     // Otros métodos necesarios para la persistencia de cuentas
-//   }
+const NAME: string = 'accounts';
+export class AccountRepository {
+    static async list(dbquery: any, showDeleted:boolean = false): Promise<AccountType[]>{
+        const db = await MongoDB.connect();
+        const result = await db.list(NAME, dbquery, showDeleted)
+        await db.close() 
+        return result
+    }
+    static async read(dbquery: any, showDeleted:boolean = false): Promise<AccountType>{
+        const db = await MongoDB.connect();
+        const result = await db.read(NAME, dbquery, showDeleted)
+        await db.close() 
+        return result
+    }
+    static async update(dbquery: any, accountData: AccountType): Promise<AccountType>{
+        const db = await MongoDB.connect();
+        const result = await db.update(NAME, dbquery, accountData)
+        await db.close() 
+        return result
+    }
+    static async create(accountData: AccountType): Promise<AccountType>{
+        const db = await MongoDB.connect();
+        const result = await db.create(NAME, accountData)
+        await db.close() 
+        return result
+    }
+    static async delete(dbquery: any): Promise<AccountType>{
+        const db = await MongoDB.connect();
+        const result = await db.delete(NAME, dbquery)
+        await db.close() 
+        return result
+    }
+  }
--- backend/src/repositories/TransactionRepository.ts
+++ backend/src/repositories/TransactionRepository.ts
@@ -1,6 +1,36 @@
-// import { Transaction } from "../models/Transaction";
+import { MongoDB } from "../connectors/db";
+import { TransactionType } from "../models/Transaction";
 
-// interface TransactionRepository {
-//     save(transaction: Transaction): Promise<void>;
-//     // Otros métodos necesarios para la persistencia de transacciones
-// }
+const NAME: string = 'transactions';
+export class TransactionRepository {
+    static async list(dbquery: any, showDeleted:boolean = false): Promise<TransactionType[]>{
+        const db = await MongoDB.connect();
+        const result = await db.list(NAME, dbquery, showDeleted)
+        await db.close() 
+        return result
+    }
+    static async read(dbquery: any, showDeleted:boolean = false): Promise<TransactionType>{
+        const db = await MongoDB.connect();
+        const result = await db.read(NAME, dbquery, showDeleted)
+        await db.close() 
+        return result
+    }
+    static async update(dbquery: any, transactionData: TransactionType): Promise<TransactionType>{
+        const db = await MongoDB.connect();
+        const result = await db.update(NAME, dbquery, transactionData)
+        await db.close() 
+        return result
+    }
+    static async create(transactionData: TransactionType): Promise<TransactionType>{
+        const db = await MongoDB.connect();
+        const result = await db.create(NAME, transactionData)
+        await db.close() 
+        return result
+    }
+    static async delete(dbquery: any): Promise<TransactionType>{
+        const db = await MongoDB.connect();
+        const result = await db.delete(NAME, dbquery)
+        await db.close() 
+        return result
+    }
+  }
