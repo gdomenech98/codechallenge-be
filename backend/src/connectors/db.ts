a670431191fbc4ee32c850dab435e63629e29d2f
Gerard <gerard@protofy.xyz>
2024-02-05 23:09:14 +0100

    [ADD] Mongo db connector class and test infra connector

--- backend/src/connectors/db.ts
+++ backend/src/connectors/db.ts
@@ -1,11 +1,40 @@
 import { MongoClient } from 'mongodb';
 
-const DB_URL = "mongodb://localhost:27017/db"
+const DB_URL = "mongodb://mongodb:27017/db"
 
-export class DB {
-    constructor(){}
-    
-    async list(collectionName: string, dbquery: any, showDeleted?: boolean) {
+export class MongoDB {
+    public client: any;
+    constructor(client: MongoClient) {
+        this.client = client;
+    }
+
+    static async _newInstance(uri = DB_URL): Promise<MongoDB> {
+        try {
+            const client: MongoClient = await new MongoClient(uri).connect()
+            return new this(client);
+        } catch (e: any) {
+            throw "Could not connect client"
+        }
+    }
+
+    static async connect(uri?: string): Promise<MongoDB> {
+        try {
+            return MongoDB._newInstance(uri)
+        } catch (e) {
+            console.error("Failed to connect to: ", uri)
+            throw e
+        }
+    }
+
+    async close(): Promise<void> {
+        await this.client.close()
+    }
+
+    private getDB() {
+        return this.client.db()
+    }
+
+    async list(collectionName: string, dbquery: any, showDeleted?: boolean): Promise<any> {
         const collection = this.getDB().collection(collectionName);
         if (!showDeleted) dbquery = { ...dbquery, _delete: { $exists: false } }
         const data = await collection.find(dbquery).toArray();
@@ -13,7 +42,7 @@ export class DB {
         return data
     }
 
-    async read(collectionName: string, dbquery: any, showDeleted?: boolean) {
+    async read(collectionName: string, dbquery: any, showDeleted?: boolean):Promise<any> {
         const collection = this.getDB().collection(collectionName);
         if (!showDeleted) dbquery = { ...dbquery, _delete: { $exists: false } }
         const data = await collection.findOne(dbquery)
@@ -21,26 +50,52 @@ export class DB {
         return data
     }
 
-    async create(collectionName: string, data: any) {
+    async create(collectionName: string, data: any): Promise<any> {
         const collection = this.getDB().collection(collectionName);
         try {
             await collection.insertOne(data)
             return data
-        } catch (e) {
+        } catch (e: any) {
             if (e.code == 11000) throw "Duplicated key"
         }
     }
 
-    async update(collectionName: string, dbquery: any, data: any) { // update One
+    async createMany(collectionName: string, data: any[]): Promise<any> {
         const collection = this.getDB().collection(collectionName);
-        let dbData;
+        try {
+            await collection.insertMany(data);
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    async update(collectionName: string, dbquery: any, data: any): Promise<any> { // update One
+        const collection = this.getDB().collection(collectionName);
+        let dbData: any;
         try {
             dbData = await collection.replaceOne(dbquery, data)
-        } catch (e) {
+        } catch (e: any) {
             if (e.code == 11000) throw "Duplicated key"
         }
         if (!dbData.matchedCount) throw "Not found"
         return data
+    }
+
+    async delete(collectionName: string, dbquery: any): Promise<any> { // Delete just adds "_delete" field to true, and read/list operations ommit these items
+        const element = await this.read(collectionName, dbquery )
+        return await this.update(collectionName, dbquery, {...element, _delete: true})
+    }
+
+    async generateIndex(collectionName: string, field: string, indexType: 'unique' | 'text'): Promise<void> { // recover one element deleted
+        const collection = this.getDB().collection(collectionName);
+        switch (indexType) {
+            case 'unique':
+                await collection.createIndex({ [field]: 1 }, { unique: true })
+                break;
+            case 'text':
+                await collection.createIndex({ [field]: "text" })
+                break;
+        }
     }
 
 }
---
+++ backend/src/test/connectors.spec.ts
@@ -0,0 +1,64 @@
+import { MongoDB } from "../connectors/db"
+import { v4 as uuidv4 } from 'uuid';
+
+describe("test db connector 'mongodb' operations", () => {
+    const COLLECTION = 'test'
+    let db: MongoDB;
+    beforeEach(async () => {
+        db = await MongoDB.connect();
+    })
+    afterEach(async () => {
+        await db.close()
+    })
+    it('should create a db instance (client)', async () => {
+        const client = db.client
+        expect(client).not.toBeUndefined()
+    })
+    it('should create a unique index for "id" field', async () => {
+        try {
+            await db.generateIndex(COLLECTION, 'id', 'unique')
+            expect(true).toBe(true)
+        } catch (error: any) {
+            expect('Error generating index').toBeFalsy() // Make sure crash when generateIndex fail
+        }
+    })
+    describe('test LCRUD operations', () => {
+        const randomId1 = uuidv4()
+        const randomId2 = uuidv4()
+        const data1 = { id: randomId1, name: 'jon' }
+        const data2 = { id: randomId2, name: 'elisse' }
+        it('should create a db document', async () => {
+            const insertedData1 = await db.create(COLLECTION, data1)
+            expect(insertedData1).toStrictEqual(data1)
+            const insertedData2 = await db.create(COLLECTION, data2)
+            expect(insertedData2).toStrictEqual(data2)
+        })
+        it('should update a db document', async () => {
+            const updatedData = { id: randomId1, name: 'bob' }
+            const insertedData = await db.update(COLLECTION, { id: randomId1 }, updatedData)
+            expect(insertedData).toStrictEqual(insertedData)
+        })
+        it('should list all db documents', async () => {
+            const insertedData = await db.list(COLLECTION, {})
+            expect(insertedData).toHaveLength(2)
+        })
+        it('should read specific db document', async () => {
+            const insertedData = await db.read(COLLECTION, { id: randomId2 })
+            expect(insertedData).toStrictEqual(data2)
+        })
+        it('should delete specific db document', async () => {
+            const deletedData2 = await db.delete(COLLECTION, { id: randomId2 })
+            expect(deletedData2).toStrictEqual({ ...data2, _delete: true })
+            expect(await db.list(COLLECTION, {})).toHaveLength(1)
+            const deletedData1 = await db.delete(COLLECTION, { id: randomId1 })
+            console.log(deletedData1)
+            expect(deletedData1).toStrictEqual({ ...deletedData1, _delete: true })
+            try{
+                await db.list(COLLECTION, {})
+                expect('List has no items, should throw an exception').toBeFalsy();
+            }catch(e){
+                expect(e).toBe('Not found')
+            }
+        })
+    })
+})
