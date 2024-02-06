305a6e7067518022cd2d04db73aa9366160eb12e
Gerard <gerard@protofy.xyz>
2024-02-06 20:59:24 +0100

    [REFACTOR] deletes duplicated code

--- backend/src/repositories/AccountRepository.ts
+++ backend/src/repositories/AccountRepository.ts
@@ -3,34 +3,40 @@ import { AccountType } from "../models/Account";
 
 const NAME: string = 'accounts';
 export class AccountRepository {
-    static async list(dbquery: any, showDeleted:boolean = false): Promise<AccountType[]>{
+    private static async connect() {
         const db = await MongoDB.connect();
+        if (!db) throw 'Could not connect to database'
+        return db;
+    }
+
+    static async list(dbquery: any, showDeleted: boolean = false): Promise<AccountType[]> {
+        const db = await AccountRepository.connect();
         const result = await db.list(NAME, dbquery, showDeleted)
-        await db.close() 
+        await db.close()
         return result
     }
-    static async read(dbquery: any, showDeleted:boolean = false): Promise<AccountType>{
-        const db = await MongoDB.connect();
+    static async read(dbquery: any, showDeleted: boolean = false): Promise<AccountType> {
+        const db = await AccountRepository.connect();
         const result = await db.read(NAME, dbquery, showDeleted)
-        await db.close() 
+        await db.close()
         return result
     }
-    static async update(dbquery: any, accountData: AccountType): Promise<AccountType>{
-        const db = await MongoDB.connect();
+    static async update(dbquery: any, accountData: AccountType): Promise<AccountType> {
+        const db = await AccountRepository.connect();
         const result = await db.update(NAME, dbquery, accountData)
-        await db.close() 
+        await db.close()
         return result
     }
-    static async create(accountData: AccountType): Promise<AccountType>{
-        const db = await MongoDB.connect();
+    static async create(accountData: AccountType): Promise<AccountType> {
+        const db = await AccountRepository.connect();
         const result = await db.create(NAME, accountData)
-        await db.close() 
+        await db.close()
         return result
     }
-    static async delete(dbquery: any): Promise<AccountType>{
-        const db = await MongoDB.connect();
+    static async delete(dbquery: any): Promise<AccountType> {
+        const db = await AccountRepository.connect();
         const result = await db.delete(NAME, dbquery)
-        await db.close() 
+        await db.close()
         return result
     }
-  }
+}
--- backend/src/repositories/TransactionRepository.ts
+++ backend/src/repositories/TransactionRepository.ts
@@ -3,34 +3,39 @@ import { TransactionType } from "../models/Transaction";
 
 const NAME: string = 'transactions';
 export class TransactionRepository {
-    static async list(dbquery: any, showDeleted:boolean = false): Promise<TransactionType[]>{
+    private static async connect() {
         const db = await MongoDB.connect();
+        if (!db) throw 'Could not connect to database'
+        return db;
+    }
+    static async list(dbquery: any, showDeleted: boolean = false): Promise<TransactionType[]> {
+        const db = await TransactionRepository.connect()
         const result = await db.list(NAME, dbquery, showDeleted)
-        await db.close() 
+        await db.close()
         return result
     }
-    static async read(dbquery: any, showDeleted:boolean = false): Promise<TransactionType>{
-        const db = await MongoDB.connect();
+    static async read(dbquery: any, showDeleted: boolean = false): Promise<TransactionType> {
+        const db = await TransactionRepository.connect()
         const result = await db.read(NAME, dbquery, showDeleted)
-        await db.close() 
+        await db.close()
         return result
     }
-    static async update(dbquery: any, transactionData: TransactionType): Promise<TransactionType>{
-        const db = await MongoDB.connect();
+    static async update(dbquery: any, transactionData: TransactionType): Promise<TransactionType> {
+        const db = await TransactionRepository.connect()
         const result = await db.update(NAME, dbquery, transactionData)
-        await db.close() 
+        await db.close()
         return result
     }
-    static async create(transactionData: TransactionType): Promise<TransactionType>{
-        const db = await MongoDB.connect();
+    static async create(transactionData: TransactionType): Promise<TransactionType> {
+        const db = await TransactionRepository.connect()
         const result = await db.create(NAME, transactionData)
-        await db.close() 
+        await db.close()
         return result
     }
-    static async delete(dbquery: any): Promise<TransactionType>{
-        const db = await MongoDB.connect();
+    static async delete(dbquery: any): Promise<TransactionType> {
+        const db = await TransactionRepository.connect()
         const result = await db.delete(NAME, dbquery)
-        await db.close() 
+        await db.close()
         return result
     }
-  }
+}
