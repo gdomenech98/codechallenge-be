5054e8f64765d0c6d9bbbf266227d94a1c26353d
Gerard <gerard@protofy.xyz>
2024-02-06 09:59:42 +0100

    [Refactor] add healthcheck validation to introduce positive amount to perform operation

--- backend/src/models/Account.ts
+++ backend/src/models/Account.ts
@@ -43,12 +43,12 @@ export class Account {
 
     set(key: keyof AccountType, value: any): Account {
         const prevData = this.getData()
-        return Account.load({...prevData, [key]: value})
+        return Account.load({ ...prevData, [key]: value })
     }
 
     static create(ownerId: string, accountId?: string): Account {
         const newAccountData: AccountType = {
-            accountId: accountId??uuidv4(),
+            accountId: accountId ?? uuidv4(),
             ownerId,
             balance: 0 // assuming that when created the balance is 0
         }
@@ -56,26 +56,30 @@ export class Account {
     }
 
     withdraw(amount: number): Account {
+        if (amount <= 0) throw 'Error: withdrawal must specify an amount'
         const updatedBalance: number = this.getBalance() - amount
         // Widthdraw validation
         const isValid: boolean = updatedBalance > this.MAX_OVERDRAFT;
-        if(!isValid) throw `Could not withdraw ammount ${amount} from the account, maximum overdraft (${this.MAX_OVERDRAFT}) exceeded`
+        if (!isValid) throw `Could not withdraw ammount ${amount} from the account, maximum overdraft (${this.MAX_OVERDRAFT}) exceeded`
         return this.set('balance', updatedBalance)
     }
 
-    deposit(amount: number, dailyDepositedAmount: number): Account { 
-        if(amount + dailyDepositedAmount > this.MAX_DEPOSIT_PER_DAY) throw `Could not diposito amount ${amount}, maximum deposit per day (${this.MAX_DEPOSIT_PER_DAY}$/24h) exceeded`
+    deposit(amount: number, dailyDepositedAmount: number): Account {
+        if (amount <= 0) throw 'Error: depostits must specify an amount'
+        if (amount + dailyDepositedAmount > this.MAX_DEPOSIT_PER_DAY) throw `Could not diposito amount ${amount}, maximum deposit per day (${this.MAX_DEPOSIT_PER_DAY}$/24h) exceeded`
         const updatedBalance: number = this.getBalance() + amount;
         return this.set('balance', updatedBalance)
     }
 
     transfer(amount: number): Account {
-        if(this.getBalance() - amount < 0) throw 'Can not overdraw in transfer'
+        if (amount <= 0) throw 'Error: transfers must specify an amount'
+        if (this.getBalance() - amount < 0) throw 'Can not overdraw in transfer'
         const updatedBalance: number = this.getBalance() - amount;
         return this.set('balance', updatedBalance)
     }
 
     recieveTransfer(amount: number): Account {
+        if (amount <= 0) throw 'Error: recieved transfers must specify an amount'
         const updatedBalance: number = this.getBalance() + amount;
         return this.set('balance', updatedBalance)
     }
