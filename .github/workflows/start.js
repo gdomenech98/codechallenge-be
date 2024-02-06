ccbb83b2e5a1620d9c58be6074fbedafd3e25059
Gerard Domènech <108338080+gdomenech98@users.noreply.github.com>
2024-02-06 15:22:59 +0100

    [ADD] healthcheck start.js script for ci

---
+++ .github/workflows/start.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const execSync = require('child_process').execSync;
+const exec = require('child_process').exec;
+
+const PROJECT_DIR = path.join(__dirname, "..", "..")
+const DEV_SERVER_URL = "http://127.0.0.1:3300/api/v1/healthcheck"
+const args = process.argv.slice(2);
+const timeout = args[0] ?? 10000;// default 10s
+const RETRY_TIME = 1000 // 1 sec
+// Run project
+const start = () => {
+    console.log('Starting project...')
+    exec(`cd ${PROJECT_DIR} && ./dev > /dev/null &`, { encoding: 'utf-8' });
+}
+
+const healthCheck = () => { // Check system is up and ready to use
+    console.log('Running healthcheck, waiting to be ready...')
+    return new Promise((resolve, reject) => {
+        const isReady = () => {
+            let isEnabled = execSync(`curl -s ${DEV_SERVER_URL} | grep -q "Running!" && echo true || echo false`, { encoding: 'utf-8' }).trim()
+            if (isEnabled === 'true') {
+                console.log('System Ready!')
+                clearInterval(interval)
+                resolve()
+            }
+            else {
+                console.log('healthchecking, please wait...')
+            }
+        }
+        let interval = setInterval(isReady, RETRY_TIME);
+        setTimeout(() => {
+            clearInterval(interval)
+            reject("Error: healthcheck timeout.")
+        }, timeout)
+    })
+}
+
+const main = async () => {
+    start()
+    await healthCheck()
+    process.exit()
+}
+main()
