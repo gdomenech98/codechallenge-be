9dca82054f3cb2bf0bf69f7a1619c7a8a4a23c70
Gerard <gerard@protofy.xyz>
2024-02-06 11:18:55 +0100

    [ADD] infra routing test endpoints

---
+++ backend/src/test/e2e/infraRouting.spec.ts
@@ -0,0 +1,164 @@
+import { v4 as uuidv4 } from 'uuid';
+import { Account, AccountType } from '../../models/Account';
+import { AccountRepository } from '../../repositories/AccountRepository';
+import { OperationType } from '../../services/OperationsService';
+import {API} from '../../connectors/API';
+
+
+describe("test endpoints", () => {
+    let accountId1: string | undefined
+    let ownerId1: string | undefined
+    let accountId2: string | undefined
+    let ownerId2: string | undefined
+    let account1: Account | undefined
+    let account2: Account | undefined
+    let response
+
+    beforeEach(async () => {
+        accountId1 = uuidv4()
+        ownerId1 = uuidv4()
+        accountId2 = uuidv4()
+        ownerId2 = uuidv4()
+        account1 = Account.create(ownerId1, accountId1)
+        account2 = Account.create(ownerId2, accountId2)
+        try {
+            await AccountRepository.create(account1.getData())
+            await AccountRepository.create(account2.getData())
+        } catch (e) {
+            throw "Could not create accounts"
+        }
+    })
+
+    it("should deposit specific amount to specific account", async () => {
+        try {
+            response = await API.post("/deposit", {
+                accountId: accountId1,
+                amount: 300
+            });
+            expect(Account.load((response as OperationType).account).getBalance()).toBe(300)
+        } catch (e) {
+            expect(e).toBeFalsy()
+        }
+    })
+
+    it("should not be able to deposit specific amount exceed daily deposit amount", async () => {
+        try {
+            response = await API.post("/deposit", {
+                accountId: accountId1,
+                amount: 30000
+            });
+            expect('Error: could not deposit overdraw').toBeFalsy()
+        } catch (e) {
+            expect(e).toBeTruthy()
+        }
+    })
+
+    it("should withdraw specific amount from specific account", async () => {
+        try {
+            // First deposit
+            await API.post("/deposit", { accountId: accountId1, amount: 200 });
+            response = await API.post("/withdraw", {
+                accountId: accountId1,
+                amount: 180
+            });
+            expect(Account.load((response as OperationType).account).getBalance()).toBe(20)
+        } catch (e) {
+            expect(e).toBeFalsy()
+        }
+    })
+
+    it("should withdraw specific and get negative but preserving max overdraft", async () => {
+        try {
+            response = await API.post("/withdraw", {
+                accountId: accountId1,
+                amount: 180
+            });
+            expect(Account.load((response as OperationType).account).getBalance()).toBe(-180)
+        } catch (e) {
+            expect(e).toBeFalsy()
+        }
+    })
+
+    it("should not withdraw specific amount due that exceed max overdraft", async () => {
+        try {
+            response = await API.post("/withdraw", {
+                accountId: accountId1,
+                amount: -280
+            });
+            expect("Error can't perform withdraw, exceeded max overdraft").toBeFalsy()
+        } catch (e) {
+            expect(e).toBeTruthy()
+        }
+    })
+
+    it("should transfer specific amount from account to another account", async () => {
+        // First deposit
+        await API.post("/deposit", { accountId: accountId1, amount: 100 });
+        try {
+            response = await API.post("/transfer", {
+                fromAccountId: accountId1,
+                toAccountId: accountId2,
+                amount: 50
+            });
+            expect(Account.load((response as OperationType).account).getBalance()).toBe(50)
+            expect(Account.load((response as OperationType).destinataryAccount as AccountType).getBalance()).toBe(50)
+
+        } catch (e) {
+            expect(e).toBeFalsy()
+        }
+    })
+
+    it("should not be able to transfer negative amount", async () => {
+        try {
+            response = await API.post("/transfer", {
+                fromAccountId: accountId1,
+                toAccountId: accountId2,
+                amount: -50
+            });
+            expect('Error could not transfer negative amount').toBeFalsy()
+        } catch (e) {
+            expect(e).toBeTruthy()
+        }
+    })
+
+    it("should not be able to transfer from  non existing account", async () => {
+        try {
+            response = await API.post("/transfer", {
+                fromAccountId: 'novalidid',
+                toAccountId: accountId2,
+                amount: 50
+            });
+            expect('Error could not transfer from provided account').toBeFalsy()
+        } catch (e) {
+            expect(e).toBeTruthy()
+        }
+    })
+
+    it("should not be able to transfer to a non existing account", async () => {
+        try {
+            response = await API.post("/transfer", {
+                fromAccountId: accountId1,
+                toAccountId: 'novalidid',
+                amount: 50
+            });
+            expect('Error could not transfer to provided account').toBeFalsy()
+        } catch (e) {
+            expect(e).toBeTruthy()
+        }
+    })
+
+    it("should not be able to transfer  the amount when over pass 'from' account balance", async () => {
+        await API.post("/deposit", { accountId: accountId1, amount: 100 });
+        try {
+            response = await API.post("/transfer", {
+                fromAccountId: accountId1,
+                toAccountId: accountId2,
+                amount: 101
+            });
+            expect('Error could not transfer the desired amount, no have enough balance').toBeFalsy()
+        } catch (e) {
+            expect(e).toBeTruthy()
+        }
+    })
+
+})
